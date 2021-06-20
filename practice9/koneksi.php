1606abf1710e819134ca8d8709114125b10b00c0
RizalAldianDA <rizalaldian99@yahoo.com>
2021-06-20 15:29:16 +0700

    change database

--- practice9/koneksi.php
+++ practice9/koneksi.php
@@ -1,12 +1,12 @@
 <?php
 	 $servername = "localhost";
-	 $username = "root";
-	 $password = "";
-	 $dbname = "db_practice9";
+	 $username = "kelasti2_prak9";
+	 $password = "pusingsekali";
+	 $dbname = "kelasti2_practice9_18102140";
 
 	 // Create connection
 	 $conn = new mysqli($servername, $username, $password, $dbname);
-	 
+
 	 // Check connection
 	 if ($conn->connect_error) {
 	 	die("Connection failed: " . $conn->connect_error);
